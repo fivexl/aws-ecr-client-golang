49ff99bded3580f737a8b4fd3ee48c54ed2409ce
Mikhail Zholobov <legal90@gmail.com>
2022-07-10 19:40:12 +0200

    Update versions of Github Actions

--- scripts/run-linter.sh
+++ scripts/run-linter.sh
@@ -4,4 +4,4 @@ set -ex
 
 TOP="$(git rev-parse --show-toplevel)"
 
-docker run --rm -v "${TOP}:/app" -w /app golangci/golangci-lint:v1.41.0 golangci-lint run -v
+docker run --rm -v "${TOP}:/app" -w /app golangci/golangci-lint:v1.46.2 golangci-lint run -v
